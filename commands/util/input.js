94c12275118c844ffbcd2addf90f554e0bacc06c
Tanish2410 <srikakolaputanish@gmail.com>
2024-11-21 23:39:15 -0500

    fixed maybe

--- commands/util/input.js
+++ commands/util/input.js
@@ -1,18 +1,19 @@
 const { SlashCommandBuilder } = require('discord.js');
+
 module.exports = {
-	data: new SlashCommandBuilder()
+    data: new SlashCommandBuilder()
         .setName('input')
-        .setDescription('you can pick a number between 1 and 6')
-        .addIntegerOption(option => {
+        .setDescription('You can pick a number between 1 and 6')
+        .addIntegerOption(option =>
             option
-            .setName("integer")
-            .setDescription("some name i guess")
-            .setMinValue(1)
-            .setMaxValue(6)
-        }),
+                .setName('integer')
+                .setDescription('Pick a number')
+                .setMinValue(1)
+                .setMaxValue(6)
+        ),
     
     async execute(interaction) {
         const userInput = interaction.options.getInteger('integer');
         await interaction.reply(`You picked ${userInput}!`);
-    },    
+    },
 };
